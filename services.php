e5a66f2fe6e72f33b96e983d28ca94005e0136ee
Tohir <thohiru.omoloye@talentbase.ng>
2018-01-29 22:50:22 +0000

    updated services page

--- services.php
+++ services.php
@@ -1,14 +1,14 @@
 <?php include "header.php"; ?>
 
 <div class="banner-container">
-<a href="#">
-    <img src="images/Uniagents_Christmas-offer.jpg" width="100%"></a>
+    <a href="#">
+        <img src="banners/jws_yearend-offer.jpg" width="100%"></a>
 </div>
 <!-- Body goes here -->
 
 
 <!--<div class="white-continer">-->
-<div class="wrapper" style="height: 500px">
+<div class="wrapper" style="height: 1200px">
     <div class="main-heading" id="underGraduate">
         <i class="fa fa-institution"></i> Services
     </div>
@@ -16,41 +16,70 @@
         <div id="underGraduate">
             <h1 class="main-heading">Under Graduate Studies</h1>
         </div>
-        
+
         <div id="postGraduate">
             <h1 class="main-heading">Post Graduate Studies</h1>
         </div>
-        
+
         <!-- Skilled worker -->
         <div id="skilledProgramme">
-            <h1 class="main-heading">Skilled Worker Programme</h1>
+            <h1 class="main-heading country-head">Skilled Worker Programme</h1>
             Numerous countries are currently choosing skilled immigrants as permanent residents based on their ability to settle in their country and take part in her economy. If you have skill and work experience, we can help you apply to immigrate to Canada, Ireland and Sweden. 
-        </div>
-        
-        <!-- Test Preparations -->
-        <div id="testPrep">
-            <h1 class="main-heading">Test Preparation Classes</h1>
-            <p>
-                Before applying for the universities we provide our student clients with a rich set of resources from learning center and online tests which helps the student to improve one's analytical, problem solving and confidence levels while taking the real exams like.
-                <ul>
-                <li>GRE</li>
-                <li>GMAT</li>
-                <li>SAT</li>
-                <li>LSAT</li>
-                <li>IELTS</li>
-                <li>TOEFL</li>
-            </ul>
-            </p>
-            
-            
-        </div>
-       
+            <img src="img/jws_work_in_canada.jpg" alt="Work in canada" style="max-height: 400px" />
         </div>
 
+        <!-- Test Preparations -->
+        
+        <div id="testPrep">
+            <h1 class="main-heading country-head">Test Preparation Classes</h1>
+            <p>
+                Before applying for the universities we provide our student clients with a rich set of resources from learning center and online tests which helps the student to improve one's analytical, problem solving and confidence levels while taking the real exams like.
+            </p>
+            <br>
+        </div>
+        <ul class="visa-listing">
+
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_gre_test.jpg" class="flag_img" alt="GRE"> GRE
+                </a>
+            </li>
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_gmat_logo.jpg" class="flag_img" alt="GMAT"> GMAT
+                </a>
+            </li>
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_act_sat.jpg" class="flag_img" alt="SAT"> SAT
+                </a>
+            </li>
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_lsat.jpeg" class="flag_img" alt="LSAT"> LSAT
+                </a>
+            </li>
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_ielts.jpg" class="flag_img" alt="IELTS"> IELTS
+                </a>
+            </li>
+            <li> 
+                <a href="#"> 
+                    <img src="img/jws_toefl.jpg" class="flag_img" alt="TOEFL"> TOEFL
+                </a>
+            </li>
+
+            
+            <div class="clearfix"></div>
+        </ul>
+
     </div>
-    <div class="body-right" align="center">
-        &nbsp;
-    </div>
+
+</div>
+<div class="body-right" align="center">
+    &nbsp;
+</div>
 
 </div>
 
@@ -59,4 +88,5 @@
 
 <!-- End of body -->
 <?php include "_enquiry_box.php"; ?>
-<?php include "footer.php";
+<?php
+include "footer.php";
