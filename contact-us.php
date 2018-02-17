1ac693c36ba062b786e26a47ef157182528d8fc5
Tohir <thohiru.omoloye@talentbase.ng>
2018-02-17 11:32:19 +0000

    updated address

--- contact-us.php
+++ contact-us.php
@@ -11,13 +11,15 @@
     <?php include "_contact_form.php"; ?>
     
     <div class="contact-right">
-        <p>Contact us directly or leave your enquiry below:</p>
-        <span class="head">For general enquiries and information please write to us:</span>
+        <p style="font-weight: bold">Contact us directly or leave your enquiry below:</p>
+        
+        <p>12 Alafia Close, Barrackes Bus Stop, Iwo Road,<br>Ibadan, Nigeria. <br>+234-7068418698, +234-8038067508</p>
+        <br>
+        <span class="head" style="font-weight: bold">For general enquiries and information please write to us:</span>
         <p><a href="mailto:info@jwsconsult.se">info@jwsconsult.se</a></p>
         <p><a href="mailto:jwsconsult@outlook.com">jwsconsult@outlook.com</a></p>
         <p>Sweden : +46-70-279-3173</p>
         <p>Ireland : +353-83-452-2705</p>
-<!--        <p>Nigeria : +234-</p>-->
 
 
     </div>
